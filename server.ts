609bd539d5c77b05a961468bf17939f6aa5feba9
Kamil <kamilzajac01@gmail.com>
2022-03-22 21:19:09 +0100

    Rest routes with ts

--- server.ts
+++ server.ts
@@ -1,5 +1,14 @@
 import express from 'express'
+import todoRoutes from './routes/todos'
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json())
+
+
+app.use(todoRoutes)
+
+
 
 app.listen(3000)
