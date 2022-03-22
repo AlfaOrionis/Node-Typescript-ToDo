5e3851a81f6b0654cfb52568fc8900cdd0e07b4c
Kamil <kamilzajac01@gmail.com>
2022-03-22 22:41:08 +0100

    Type casting

---
+++ routes/todos.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+let todos = [];
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const body = req.body;
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ todo: newTodo, todos: todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const body = req.body;
+    const params = req.params;
+    const tid = params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
+        return res.status(200).json({ message: 'Updated todo', todos: todos });
+    }
+    res.status(404).json({ message: 'Could not find such a todo.' });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    const params = req.params;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
+    return res.status(200).json({ message: 'Todo has been removed', todos: todos });
+});
+exports.default = router;
--- routes/todos.ts
+++ routes/todos.ts
@@ -4,7 +4,10 @@ import {Todo} from '../models/todo.models'
 
 const router = Router()
 
-const todos: Todo[] = []
+type RequestBody = { text: string }
+type RequestParams = { todoId: string}
+
+let todos: Todo[] = []
 
 
 router.get('/', (req,res,next) => {
@@ -12,17 +15,54 @@ res.status(200).json({todos: todos})
 })
 
 router.post('/todo', (req,res,next) => {
+
+
+const body = req.body as RequestBody
+
+
 const newTodo: Todo = {
 id: new Date().toISOString(),
-text:req.body.text }; 
+text: body.text } 
 
 todos.push(newTodo)
 
-} )
+res.status(201).json({todo: newTodo, todos: todos})
+
+
+}
 
 
 
+)
 
+
+ router.put('/todo/:todoId', (req,res,next) => {
+    const body = req.body as RequestBody
+    const params = req.params as RequestParams
+
+     const tid = params.todoId;
+    
+     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid)
+    
+     if(todoIndex >= 0) {
+     todos[todoIndex] = {id: todos[todoIndex].id, text: body.text};
+     return res.status(200).json({message: 'Updated todo', todos: todos})
+    
+    
+     } 
+    
+     res.status(404).json({ message: 'Could not find such a todo.'})
+    
+    
+     } )
+     
+
+router.delete('/todo/:todoId', (req,res,next)=> {
+    const params = req.params as RequestParams
+todos = todos.filter(todoItem => todoItem.id !== params.todoId)
+
+return res.status(200).json({message: 'Todo has been removed', todos: todos})
+})
 
 
 export default router
